61b53ec9755971480ad0571b08da1cf73c726b6a
Jack O'Neill <43734109+ojack123@users.noreply.github.com>
2019-04-10 14:32:19 -0400

    Created an iterative psi_0 solver

--- homework_11_ode.m
+++ homework_11_ode.m
@@ -15,8 +15,8 @@ function states_dot = homework_11_ode(states,cost,g1,g2,dw_x_dx,dw_x_dy,dw_y_dx,
     dw_y_dx = dw_y_dx(x_index,y_index);
     dw_y_dy = dw_y_dy(x_index,y_index);
     
-    x_dot   = (0.1)*cos(psi) + w_x;
-    y_dot   = (0.1)*sin(psi) + w_y;
+    x_dot   = (0.75)*cos(psi) + w_x;
+    y_dot   = (0.75)*sin(psi) + w_y;
     psi_dot = dw_y_dx*sin(psi)^2 - dw_x_dy*cos(psi)^2 + ...
               (dw_x_dx - dw_y_dy)*sin(psi)*cos(psi);
     
