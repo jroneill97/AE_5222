bb75d212f47c79b955c838705f783c10a80ef23a
ojack123 <ojack123@gmail.com>
2019-04-17 21:40:11 -0400

    Update homework_10_linear_equations.m

--- homework_10_linear_equations.m
+++ homework_10_linear_equations.m
@@ -1,41 +1,41 @@
-function f = homework_10_linear_equations(states_)
+function funs = homework_10_linear_equations(states)
 
 %% x1
-f(1) = states_(1)  - 5;
-f(2) = states_(2)  - states_(7);
-f(3) = states_(3)  - states_(8);
-f(4) = states_(4)  - states_(9);
-f(5) = states_(5)  - states_(10);
-f(6) = states_(6)  - states_(11);
+funs(1) = states(1) - 5;
+funs(2) = states(2) - states(7);
+funs(3) = states(3) - states(8);
+funs(4) = states(4) - states(9);
+funs(5) = states(5) - states(10);
+funs(6) = states(6) - states(11);
 
 %% x2
-f(7) = states_(7)  - 2;
-f(8) = states_(8)  - states_(25);
-f(9) = states_(9)  - states_(26);
-f(10) = states_(10) - states_(27);
-f(11) = states_(11) - states_(28);
-f(12) = states_(12) - states_(29);
+funs(7) = states(7) - 2;
+funs(8) = states(8) - states(25);
+funs(9) = states(9) - states(26);
+funs(10) = states(10) - states(27);
+funs(11) = states(11) - states(28);
+funs(12) = states(12) - states(29);
 
 %% p1
-f(13) = states_(13) - 0.1 * states_(1);
-f(14) = states_(14) - 0.1 * states_(2);
-f(15) = states_(15) - 0.1 * states_(3);
-f(16) = states_(16) - 0.1 * states_(4);
-f(17) = states_(17) - 0.1 * states_(5);
-f(18) = states_(18) - 0.1 * states_(6);
+funs(13) = states(13) - 0.1 * states(1);
+funs(14) = states(14) - 0.1 * states(2);
+funs(15) = states(15) - 0.1 * states(3);
+funs(16) = states(16) - 0.1 * states(4);
+funs(17) = states(17) - 0.1 * states(5);
+funs(18) = states(18) - 0.1 * states(6);
 
 %% p2
-f(19) = states_(19) - 0.05 * states_(7) + states_(13);
-f(20) = states_(20) - 0.05 * states_(8) + states_(14);
-f(21) = states_(21) - 0.05 * states_(9) + states_(15);
-f(22) = states_(22) - 0.05 * states_(10) + states_(16);
-f(23) = states_(23) - 0.05 * states_(11) + states_(17);
-f(24) = states_(24) - 0.05 * states_(13);
+funs(19) = states(19) - 0.05 * states(7) + states(13);
+funs(20) = states(20) - 0.05 * states(8) + states(14);
+funs(21) = states(21) - 0.05 * states(9) + states(15);
+funs(22) = states(22) - 0.05 * states(10) + states(16);
+funs(23) = states(23) - 0.05 * states(11) + states(17);
+funs(24) = states(24) - 0.05 * states(13);
 
 %% u
-f(25) = states_(25) - states_(20);
-f(26) = states_(26) - states_(21);
-f(27) = states_(27) - states_(22);
-f(28) = states_(28) - states_(23);
-f(29) = states_(29) - states_(24);
+funs(25) = states(25) - states(20);
+funs(26) = states(26) - states(21);
+funs(27) = states(27) - states(22);
+funs(28) = states(28) - states(23);
+funs(29) = states(29) - states(24);
 
